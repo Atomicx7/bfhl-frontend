007503cc78f29261d0dbad469fb08685ddaee0a9
Yash <145636761+Atomicx7@users.noreply.github.com>
2025-02-21 19:14:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       }
 
       // POST to your backend
-      const res = await fetch('http://localhost:3000/bfhl', {
+      const res = await fetch('http://bfhl-backend-psi-three.vercel.app/bfhl', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: inputValue,
